749f77abb02836894aa6100f8c63b5ce5bbb18ca
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-09-05 02:29:42 -0500

    Modificacion
    Modificación a las clases asignatura y grupo

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,30 +3,34 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = None
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes = []):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
+        self.grado = 'Grado 12'
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
     def agregarAlumno(self, alumno, lista=[]):
-        lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
+        self.listadoAlumnos.append(alumno)
 
-    # def __str__(self):
-    #     pass
+
+    def __str__(self):
+        return f'Grupo de estudiantes: {self._grupo}'
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 4"):
+        cls.grado = nombre
+
+
+    @ classmethod
+    def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
